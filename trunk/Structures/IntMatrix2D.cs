1320d01272d5fbfa587146833fc565dfdd2dcf52
m.sandacz@o2.pl <m.sandacz@o2.pl@3e6c9bec-0eb1-6713-cf89-4e78175036ce>
2011-11-10 23:36:43 +0000

    zmieniono nazwę i typ danych macierzy

--- trunk/Structures/IntMatrix2D.cs
+++ trunk/Structures/IntMatrix2D.cs
@@ -5,14 +5,14 @@ using System.Text;
 
 namespace Structures
 {
-    public class Matrix2D
+    public class IntMatrix2D
     {
-        private float[,] m;
-        public Matrix2D(int n)
+        private int[,] m;
+        public IntMatrix2D(int n)
         {
-            m = new float[n,n];
+            m = new int[n,n];
         }
-        public float this[int i, int j]
+        public int this[int i, int j]
         {
             get { return m[i, j]; }
             set { m[i, j] = value; }
