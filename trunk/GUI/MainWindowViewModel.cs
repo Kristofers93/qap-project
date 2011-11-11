6aac2c6814ff9f8626e1db29cb8e0960e210a68e
m.sandacz@o2.pl <m.sandacz@o2.pl@3e6c9bec-0eb1-6713-cf89-4e78175036ce>
2011-11-11 10:06:15 +0000

    dodanie komend do przycisków

---
+++ trunk/GUI/MainWindowViewModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Input;
+
+namespace GUI
+{
+    class MainWindowViewModel
+    {
+        public void LoadData()
+        {
+            //TODO
+            System.Windows.MessageBox.Show("Nie zaimplementowano :P");
+        }
+
+        public void Run()
+        {
+            //TODO
+            System.Windows.MessageBox.Show("Nie zaimplementowano :P");
+        }
+
+        public ICommand LoadDataCommand
+        {
+            get { return new RelayCommand(LoadData); }
+        }
+
+        public ICommand RunCommand
+        {
+            get { return new RelayCommand(Run); }
+        }
+
+    }
+}
--- trunk/GUI/Results.xaml.cs
+++ trunk/GUI/Results.xaml.cs
@@ -9,21 +9,18 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace GUI
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for Results.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class Results : Window
     {
-        MainWindowViewModel _viewModel = new MainWindowViewModel();
-        public MainWindow()
+        public Results()
         {
             InitializeComponent();
-            LayoutRoot.DataContext = _viewModel;
         }
     }
 }
