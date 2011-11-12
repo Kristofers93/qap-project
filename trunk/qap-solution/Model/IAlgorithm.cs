731a8a94f67c9b13c86e34a5d2c6381310a71e4e
tomek.godzik@gmail.com <tomek.godzik@gmail.com@3e6c9bec-0eb1-6713-cf89-4e78175036ce>
2011-11-12 17:02:26 +0000

    Dodane metody interfejsu dla algorytmów.

--- trunk/qap-solution/Model/IAlgorithm.cs
+++ trunk/qap-solution/Model/IAlgorithm.cs
@@ -2,11 +2,26 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Collections;
+using System.Collections.Generic;
 
 namespace Model
 {
     interface IAlgorithm
     {
-        public void ReturnOptimalResult();
+        //lista rozwiazan, czyli najbardziej optymalne rozwiazanie
+        public List<int> ReturnOptimalResult();
+
+        //najmniejszy zwracany koszt
+        public int getMinimalCost();
+
+        //koszt dla danej iteracji
+        public int getCurrentCost(int iterationNumber);
+
+        //zwraca liste parametrów
+        public List<string> getParameterNames();
+
+        //zwraca słownik zawierający wszystkie wartości domyślne parametrów
+        public Dictionary<string, double> getParameterValues();
     }
 }
