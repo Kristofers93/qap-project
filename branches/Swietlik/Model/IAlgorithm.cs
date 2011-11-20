6e3d088277d694569072579199186c40fbb526c2
pr.anielski@gmail.com <pr.anielski@gmail.com@3e6c9bec-0eb1-6713-cf89-4e78175036ce>
2011-11-20 18:16:11 +0000

    uda sie? nie uda?

---
+++ branches/Swietlik/Model/IAlgorithm.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Model
+{
+    interface IAlgorithm
+    {
+        //lista reprezentujaca najbardziej optymalne rozwiazanie
+        public List<int> ReturnMinimalResult();
+
+        //najmniejszy zwracany koszt
+        public int GetMinimalCost();
+
+        //koszt dla danej iteracji
+        public int GetCurrentCost(int iterationNumber);
+
+        //zwraca liste parametrow
+        public static List<string> GetParameterNames();
+
+        //zwraca słownik zawierający wszystkie wartości domyślne parametrów
+        public static Dictionary<string, double> GetParameterValues();
+
+        //ustawianie niedomyślnych parametrów, klucz - nazwa,
+        public void SetParameters(Dictionary<string, double> parameters);
+
+
+        //ustawianie danych, macierze A i B z modelu i ich rozmiar,
+        public void SetTestData(int[,] A, int[,] B, int numberOfInstances);
+
+        //uruchomienie obliczen, zwraca 0 w przypadku konca obliczen?
+        public int runAlgorithm();
+    }
+
+}
