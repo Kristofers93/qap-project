71e4e01a9dc9902ccbc451f0f9f3afa06a9bca5b
cinkrupa@gmail.com <cinkrupa@gmail.com@3e6c9bec-0eb1-6713-cf89-4e78175036ce>
2011-11-22 22:05:26 +0000

    jakiś algorytm pszczeli, ale trzeba go dopasować do naszego problemu:P

--- trunk/qap-solution/Model.Bee/BeeAlgorithm.cs
+++ trunk/qap-solution/Model.Bee/BeeAlgorithm.cs
@@ -7,5 +7,47 @@ namespace Model.Bee
 {
     public class BeeAlgorithm : IAlgorithm
     {
+
+        public void runAlgorithm()
+        {
+            try
+            {
+                Console.WriteLine("\nBegin Simulated Bee Colony algorithm demo\n");
+                Console.WriteLine("Loading cities data for SBC Traveling Salesman Problem analysis");
+                TestData data = new TestData(20);
+                Console.WriteLine(data.ToString());
+                Console.WriteLine("Number of places = " + data.places.Length);
+                Console.WriteLine("Number of possible paths = " + data.NumberOfPossiblePaths().ToString("#,###"));
+                Console.WriteLine("Best possible solution (shortest path) length = " + data.ShortestPathLength().ToString("F4"));
+
+                int totalNumberBees = 100;
+                int numberInactive = 20;
+                int numberActive = 50;
+                int numberScout = 30;
+                int maxNumberVisits = 100;
+                int maxNumberCycles = 3460;
+
+                Hive hive = new Hive(totalNumberBees, numberInactive, numberActive, numberScout, maxNumberVisits, maxNumberCycles, data);
+                Console.WriteLine("\nInitial random hive");
+                Console.WriteLine(hive);
+
+                bool doProgressBar = true;
+                hive.Solve(doProgressBar);
+
+                Console.WriteLine("\nFinal hive");
+                Console.WriteLine(hive);
+
+                Console.WriteLine("End Simulated Bee Colony demo");
+                Console.ReadLine();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Fatal: " + ex.Message);
+                Console.ReadLine();
+            }
+
+        }
+
+
     }
 }
---
+++ trunk/qap-solution/Model.Bee/TestData.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Model.Bee
+{
+    class TestData
+    {
+        public char[] places;
+
+        public TestData(int numberPlaces)
+        {
+            this.places = new char[numberPlaces];
+            this.places[0] = 'A';
+            for (int i = 1; i < this.places.Length; ++i)
+                this.places[i] = (char)(this.places[i - 1] + 1);
+        }
+        public double Distance(char firstPlace, char secondPlace)
+        {
+            if (firstPlace < secondPlace)
+                return 1.0 * ((int)secondPlace - (int)firstPlace);
+            else
+                return 1.5 * ((int)firstPlace - (int)secondPlace);
+        }
+        public double ShortestPathLength()
+        {
+            return 1.0 * (this.places.Length - 1);
+        }
+        public long NumberOfPossiblePaths()
+        {
+            long n = this.places.Length;
+            long answer = 1;
+            for (int i = 1; i <= n; ++i)
+                checked { answer *= i; }
+            return answer;
+        }
+        public override string ToString()
+        {
+            string s = "";
+            s += "Places: ";
+            for (int i = 0; i < this.places.Length; ++i)
+                s += this.places[i] + " ";
+            return s;
+        }
+    } // class TestData
+}
