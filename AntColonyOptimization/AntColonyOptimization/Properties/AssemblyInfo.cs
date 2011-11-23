0e58a3b9709adc603fc22a99c947b99a3ec65c36
barteklechster <barteklechster@gmail.com>
2011-11-23 17:55:18 +0000

    Clear project

--- AntColonyOptimization/AntColonyOptimization/Properties/AssemblyInfo.cs
+++ AntColonyOptimization/AntColonyOptimization/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Model.Bee")]
+[assembly: AssemblyTitle("AntColonyOptimization")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Model.Bee")]
+[assembly: AssemblyProduct("AntColonyOptimization")]
 [assembly: AssemblyCopyright("Copyright ©  2011")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("3a20a973-9ce9-4fe2-8597-32dfec04331a")]
+[assembly: Guid("5d296797-2387-4376-af33-e3a5968e449f")]
 
 // Version information for an assembly consists of the following four values:
 //
