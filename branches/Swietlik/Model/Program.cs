566f661c43cba51d1b8c13ac36124e1abdcf9ee6
bogna.bb@gmail.com <bogna.bb@gmail.com@3e6c9bec-0eb1-6713-cf89-4e78175036ce>
2011-11-23 17:29:17 +0000

    swietlik : w trakcie kroku beta.

--- branches/Swietlik/Model/Program.cs
+++ branches/Swietlik/Model/Program.cs
@@ -9,8 +9,14 @@ namespace Model
     {
         static void Main(string[] args)
         {
+            List<int> l = Enumerable.Range(0, 15).ToList();
+            foreach (var i in l)
+            {
+                Console.WriteLine(i);
+            }
 
             
+
         }
     }
 }
