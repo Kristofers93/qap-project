49b7e5c789da225e99329dbaea8a532f3632245e
tomek.godzik@gmail.com <tomek.godzik@gmail.com@3e6c9bec-0eb1-6713-cf89-4e78175036ce>
2011-11-24 10:56:35 +0000

    dodana klasa parametr.

--- trunk/qap-solution/Model/Algorithm.cs
+++ trunk/qap-solution/Model/Algorithm.cs
@@ -5,8 +5,33 @@ using System.Text;
 
 namespace Model
 {
-    public abstract class Algorithm : IAlgorithm
+    public abstract class Algorithm
     {
-        List<Parameter> Parameters;
+        public List<Parameter> Parameters { get; set; }
+
+        //lista reprezentujaca najbardziej optymalne rozwiazanie
+        abstract public List<int> ReturnMinimalResult();
+
+        //najmniejszy zwracany koszt
+        abstract public int GetMinimalCost();
+
+        //koszt dla pierwszych n(numberOfIterations) iteracji
+        abstract public List<int> GetCosts(int numberOfIterations);
+
+        //zwraca liste parametrow
+        abstract public List<string> GetParameterNames();
+
+        //zwraca słownik zawierający wszystkie wartości domyślne parametrów
+        abstract public Dictionary<string, double> GetParameterValues();
+
+        //ustawianie niedomyślnych parametrów, klucz - nazwa,
+        abstract public void SetParameters(Dictionary<string, double> parameters);
+
+
+        //ustawianie danych, macierze A i B z modelu i ich rozmiar,
+        abstract public void SetTestData(int[,] A, int[,] B, int numberOfInstances);
+
+        //uruchomienie obliczen
+        abstract public void runAlgorithm();
     }
 }
