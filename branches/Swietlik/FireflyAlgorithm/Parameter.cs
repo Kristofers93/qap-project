b80b8f105cf2fc5077ce55f24cc04952295f5be2
bogna.bb@gmail.com <bogna.bb@gmail.com@3e6c9bec-0eb1-6713-cf89-4e78175036ce>
2011-11-24 13:23:45 +0000

    dodane pliki z Modelu.

---
+++ branches/Swietlik/FireflyAlgorithm/Parameter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Model
+{
+    public abstract class Parameter
+    {
+
+        public string Name { get; protected set; }
+
+        public abstract bool IsValid();
+
+    }
+
+    public abstract class Parameter<T> : Parameter
+    {
+        public T Value;
+
+
+        public Parameter(string _name, T _value)
+        {
+            this.Name = _name;
+            this.Value = _value;
+
+        }
+
+
+    }
+
+
+}
