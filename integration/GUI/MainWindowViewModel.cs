8e7993743a795cbe729a8bbae4d9364d3f5ecf89
tomek.godzik <tomek.godzik@gmail.com>
2011-12-28 12:10:33 +0000

    integracja

---
+++ integration/GUI/MainWindowViewModel.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.IO;
+using System.Windows;
+using System.Windows.Input;
+using Microsoft.Win32;
+using Model;
+
+namespace GUI
+{
+    internal class MainWindowViewModel
+    {
+        private int[,] A;
+        private int[,] B;
+        private int size;
+
+        private FireflyAlgorithm fireflyAlgorithm = new FireflyAlgorithm()
+        {
+            M = 10,
+            Imax = 100,
+            Gamma = 1.0,
+            Alfa =2
+        };
+
+        private BeesAlgorithm beeAlgorithm = new BeesAlgorithm()
+        {
+            Nb=10,
+            M=10,
+            Imax=100,
+            E=3,
+            Nep=40,
+            Nsp=20,
+            Ngh=0.2
+
+        };
+
+        public AntColony _antColony = new AntColony(10, 100, (float)2.0, (float)2.0, (float)0.5, (float)3.0, (float)1.0, (float)1.0, (float)5.0);
+        
+        public FireflyAlgorithm FireflyAlgorithm
+        {
+            get { return fireflyAlgorithm; }
+            set { fireflyAlgorithm = value; }
+        }
+
+        public BeesAlgorithm BeeAlgorithm
+        {
+            get { return beeAlgorithm; }
+            set { beeAlgorithm = value; }
+        }
+
+        public AntColony antColony
+        {
+            get { return _antColony; }
+            set { _antColony = value; }
+        }
+
+        public int SelectedTab { get; set; }
+
+        
+   
+
+        public ICommand LoadDataCommand
+        {
+            get { return new RelayCommand(LoadData); }
+        }
+
+        public ICommand RunCommand
+        {
+            get { return new RelayCommand(Run); }
+        }
+
+        public void LoadData()
+        {
+            // Configure open file dialog box
+            var dlg = new OpenFileDialog();
+            //dlg.FileName = "Dane testowe"; // Default file name
+            dlg.DefaultExt = ".dat"; // Default file extension
+            dlg.Filter = "Dane wejściowe|*.dat"; // Filter files by extension
+
+            // Show open file dialog box
+            bool? result = dlg.ShowDialog();
+
+            // Process open file dialog box results
+            if (result == true)
+            {
+                // Open document
+                string filename = dlg.FileName;
+
+                // create reader & open file
+                var tr = new StreamReader(filename);
+
+                // read a line of text
+                A = null;
+                int i = 0;
+                int j = 0;
+                int m = 0;
+                while (!tr.EndOfStream)
+                {
+                    string[] line = tr.ReadLine().Split(' ');
+                    foreach (string s in line)
+                    {
+                        int a;
+                        if (Int32.TryParse(s, out a))
+                        {
+                            if (A == null)
+                            {
+                                A = new int[a,a];
+                                B = new int[a,a];
+                                size = a;
+                            }
+                            else
+                            {
+                                if (m == 0) A[j, i] = a;
+                                if (m == 1) B[j, i] = a;
+                                ++i;
+                                if (i == size)
+                                {
+                                    i = 0;
+                                    ++j;
+                                    if (j == size)
+                                    {
+                                        i = 0;
+                                        j = 0;
+                                        ++m;
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+                // close the stream
+                tr.Close();
+            }
+        }
+
+        public void Run()
+        {
+            if (A == null)
+            {
+                MessageBox.Show("Załaduj najpierw dane");
+                return;
+            }
+            IAlgorithm algorithm = null; //TODO
+            switch (SelectedTab)
+            {
+                case 0:
+                    algorithm = new AntColony(_antColony.Ants,_antColony.MaxAssigns,_antColony.Alpha,_antColony.Beta,_antColony.Rho,_antColony.q,_antColony.Q0,_antColony.T0,_antColony.Q);
+                
+                    break;
+                case 1:
+                    algorithm = new BeesAlgorithm()
+                    {
+                        M = BeeAlgorithm.M,
+                        Imax = BeeAlgorithm.Imax,
+                        E = beeAlgorithm.E,
+                        Ngh=beeAlgorithm.Ngh,
+                        Nsp=beeAlgorithm.Nsp,
+                        Nb=beeAlgorithm.Nb,
+                        Nep=beeAlgorithm.Nep
+                    };
+                    break;
+                case 2:
+                    algorithm=new FireflyAlgorithm()
+                        {
+                            M = FireflyAlgorithm.M,
+                            Imax = FireflyAlgorithm.Imax,
+                            Gamma = FireflyAlgorithm.Gamma,
+                            Alfa = FireflyAlgorithm.Alfa
+                        };
+                    break;
+            }
+
+            if (algorithm == null)
+            {
+                MessageBox.Show("Nie załadowano algorytmu.");
+                return;
+            }
+
+            algorithm.SetTestData((int[,]) A.Clone(), (int[,]) B.Clone(), size);
+            var sth = new Chart(algorithm);
+            sth.Show();
+        }
+    }
+}
--- integration/Model/IAlgorithm.cs
+++ integration/Model/IAlgorithm.cs
@@ -2,33 +2,30 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Collections;
-using System.Collections.Generic;
 
 namespace Model
 {
-    interface IAlgorithm
+    public interface IAlgorithm
     {
         //lista reprezentujaca najbardziej optymalne rozwiazanie
         List<int> ReturnMinimalResult();
 
-        //najmniejszy zwracany koszt - czyli nasz wynik koszt najbardziej optymalnego rozwiazania
+        //najmniejszy zwracany koszt
         int GetMinimalCost();
-
-        //koszt dla pierwszych n(numberOfIterations) iteracji
-        List<int> GetCosts(int numberOfIterations);
-
+        
         //zwraca liste parametrow
         List<string> GetParameterNames();
 
         //zwraca słownik zawierający wszystkie wartości domyślne parametrów
         Dictionary<string, double> GetParameterValues();
-        
+
+        int GetCurrentCost();
+
+
         //ustawianie danych, macierze A i B z modelu i ich rozmiar,
         void SetTestData(int[,] A, int[,] B, int numberOfInstances);
 
         //uruchomienie obliczen
-        void runAlgorithm();
+        void RunAlgorithm();
     }
-
 }
