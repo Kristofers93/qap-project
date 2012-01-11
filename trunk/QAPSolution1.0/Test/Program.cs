09cc98abaca83d596e227f49f561d9b7d007514b
bogna.bb@gmail.com <bogna.bb@gmail.com@3e6c9bec-0eb1-6713-cf89-4e78175036ce>
2012-01-11 13:52:54 +0000

    Testowanie.

---
+++ trunk/QAPSolution1.0/Test/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FireflyTest faTest = new FireflyTest();
+            faTest.RunTest("D:\\Dokumenty\\Visual Studio\\integration\\QAPSolution1.0\\TestInstances\\bur26a.dat");
+
+            Console.ReadKey();
+        }
+    }
+}
